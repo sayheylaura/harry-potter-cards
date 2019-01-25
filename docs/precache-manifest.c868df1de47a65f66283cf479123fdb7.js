4ea65ee93efd648c5e325c5890abffd2c9fafb9d
lauraferrandof <lauraferrandof@gmail.com>
2019-01-25 19:55:51 +0100

    Create docs folder

---
+++ docs/precache-manifest.c868df1de47a65f66283cf479123fdb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "3f4aff7e0c6745b874bd",
+    "url": "./static/js/main.3f4aff7e.chunk.js"
+  },
+  {
+    "revision": "45f2e9d19631b34d4fb8",
+    "url": "./static/js/1.45f2e9d1.chunk.js"
+  },
+  {
+    "revision": "3f4aff7e0c6745b874bd",
+    "url": "./static/css/main.87b957e7.chunk.css"
+  },
+  {
+    "revision": "35447e4170a14201217575c786903058",
+    "url": "./index.html"
+  }
+];
